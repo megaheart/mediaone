9a27505439a335e5769b631bc00b47e1e1cc1d7b
Genshin <thanhlam02412002@gmail.com>
2022-05-08 23:21:48 +0700

    omg

--- src/main/java/com/shopkeeper/lam/models/jobOfPerson.java
+++ src/main/java/com/shopkeeper/lam/models/jobOfPerson.java
@@ -1,5 +1,28 @@
 package com.shopkeeper.lam.models;
 
 public enum jobOfPerson {
-   Singer,Actor;
+   SINGER("singer",1),ACTOR("actor",2);
+
+   private String name;
+   private int number;
+   jobOfPerson(String name,int number){
+      this.name=name;
+      this.number=number;
+   }
+
+   public void setName(String name) {
+      this.name = name;
+   }
+
+   public String getName() {
+      return name;
+   }
+
+   public int getNumber() {
+      return number;
+   }
+
+   public void setNumber(int number) {
+      this.number = number;
+   }
 }
