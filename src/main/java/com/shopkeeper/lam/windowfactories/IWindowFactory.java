4475a2d96a960a9128f77ac386a528ec5eac474f
Genshin <thanhlam02412002@gmail.com>
2022-05-12 11:42:50 +0700

    omg

--- src/main/java/com/shopkeeper/lam/windowfactories/IWindowFactory.java
+++ src/main/java/com/shopkeeper/lam/windowfactories/IWindowFactory.java
@@ -4,6 +4,10 @@ import javafx.fxml.FXML;
 import javafx.stage.Stage;
 
 public interface IWindowFactory {
+    public Stage currentWindow = null;
+    public Stage createWindow();
+    public Stage openWindow();
+    public Stage closeWindow();
 
 
 }
