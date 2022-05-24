c7fd8ce78f8b9932a3536efaf9b37cbc7dfbd610
DucMinh <minhmeo7a1@gmail.com>
2022-05-24 11:49:22 +0700

    Remove staff class

--- src/main/java/com/shopkeeper/minh/models/IBill.java
+++ src/main/java/com/shopkeeper/minh/models/IBill.java
@@ -3,11 +3,11 @@ package com.shopkeeper.minh.models;
 import java.time.LocalDate;
 
 public interface IBill {
-    public String getName();
-    public BillType getBillType();
-    public double getPrice();
-    public String getBillId();
-    public LocalDate getTime();
-    public boolean getIsEffected();
-    public String getNote();
+    String getName();
+    BillType getBillType();
+    double getPrice();
+    String getBillId();
+    LocalDate getTime();
+    boolean getIsEffected();
+    String getNote();
 }
