3a2e232544802c5f7b72b1e32fe21cb043f14fbf
Genshin <thanhlam02412002@gmail.com>
2022-05-24 15:34:44 +0700

    hello world

--- src/main/java/com/shopkeeper/lam/models/IBill.java
+++ src/main/java/com/shopkeeper/lam/models/IBill.java
@@ -1,14 +1,29 @@
-package com.shopkeeper.lam.models;
+package com.shopkeeper.mediaone.models;
 
 import java.time.LocalDate;
 
 public interface IBill {
-    public abstract String getName();
-    public abstract double getPrice();
-    public abstract BillType getBillType();
-    public abstract String getBillId();
-    public abstract LocalDate getTime();
-    public abstract boolean getIsEffected();
-    public abstract String getNote();
-
+    // ----------- << method.annotations@AAAAAAGAg9lfTVJENPE= >>
+    // ----------- >>
+    String getName();
+    // ----------- << method.annotations@AAAAAAGAg9qx7VJQ6yQ= >>
+    // ----------- >>
+    BillType getBillType();
+    // ----------- << method.annotations@AAAAAAGAg952L2BvAnk= >>
+    // ----------- >>
+    double getPrice();
+    // ----------- << method.annotations@AAAAAAGAg9+ThmB+lZ8= >>
+    // ----------- >>
+    int getBillId();
+    // ----------- << method.annotations@AAAAAAGAg+CozmCbsL0= >>
+    // ----------- >>
+    LocalDate getTime();
+    // ----------- << method.annotations@AAAAAAGAg+KKLWC4GcM= >>
+    // ----------- >>
+    boolean getIsEffected();
+    // ----------- << method.annotations@AAAAAAGAg+QiVmDYWF4= >>
+    // ----------- >>
+    String getNote();
+// ----------- << interface.extras@AAAAAAGAgzbV/FBSND4= >>
+// ----------- >>
 }
