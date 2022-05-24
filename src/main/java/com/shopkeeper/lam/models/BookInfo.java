e6c1d4ebcee47a4a6177fcc68d648934e9c55384
Genshin <thanhlam02412002@gmail.com>
2022-05-24 15:49:45 +0700

    hello world

--- src/main/java/com/shopkeeper/lam/models/BookInfo.java
+++ src/main/java/com/shopkeeper/lam/models/BookInfo.java
@@ -3,4 +3,11 @@ package com.shopkeeper.lam.models;
 public final class BookInfo extends ProductInfo{
     private ProductInfo productInfo;
 
+    public ProductInfo getProductInfo() {
+        return productInfo;
+    }
+
+    public void setProductInfo(ProductInfo productInfo) {
+        this.productInfo = productInfo;
+    }
 }
