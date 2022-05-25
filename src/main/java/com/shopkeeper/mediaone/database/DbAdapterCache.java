2568e7e6cab4cb040a28be3b1da2ac4a913a33ec
megaheart <21360138+megaheart@users.noreply.github.com>
2022-05-25 09:04:51 +0700

    :)))))))

--- src/main/java/com/shopkeeper/mediaone/database/DbAdapterCache.java
+++ src/main/java/com/shopkeeper/mediaone/database/DbAdapterCache.java
@@ -1,26 +1,114 @@
 package com.shopkeeper.mediaone.database;
 
 import java.util.ArrayList;
+
+import com.shopkeeper.lam.models.*;
 import com.shopkeeper.linh.models.*;
+import com.shopkeeper.minh.models.*;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 
 public class DbAdapterCache {
+    //region linh
     private ObservableList<Staff> staffs;
+    private Settings settings;
+    private ObservableList<Customer> customers;
+    private ObservableList<SaleBill> saleBills;
+    private ObservableList<Feedback> feedbacks;
+    //endregion
+    //region lam
+    private ObservableList<Publisher> publishers;
+    private ObservableList<Category> categories;
+    private ObservableList<Person> people;
+    private ObservableList<ProductInfo> productInfos;
+    private ObservableList<Product> products;
+    //endregion
+    //region minh
+    private ObservableList<ImportBill> importBills;
+    private ObservableList<OtherBill> otherBills;
+    private ObservableList<StaffBill> staffBills;
+    private ObservableList<TimeKeeping> timeKeepings;
+    private ObservableList<Shift> shifts;
+    //endregion
     public ObservableList<Staff> getStaffs(){
         return staffs;
     }
-    private Settings settings;
     public Settings getSettings(){
         return settings;
     }
-    private ObservableList<Customer> customers;
     public ObservableList<Customer> getCustomers(){
         return customers;
     }
+
+    public ObservableList<SaleBill> getSaleBills() {
+        return saleBills;
+    }
+
+    public ObservableList<Feedback> getFeedbacks() {
+        return feedbacks;
+    }
+
+    public ObservableList<Publisher> getPublishers() {
+        return publishers;
+    }
+
+    public ObservableList<Category> getCategories() {
+        return categories;
+    }
+
+    public ObservableList<Person> getPeople() {
+        return people;
+    }
+
+    public ObservableList<ProductInfo> getProductInfos() {
+        return productInfos;
+    }
+
+    public ObservableList<Product> getProducts() {
+        return products;
+    }
+
+    public ObservableList<ImportBill> getImportBills() {
+        return importBills;
+    }
+
+    public ObservableList<OtherBill> getOtherBills() {
+        return otherBills;
+    }
+
+    public ObservableList<StaffBill> getStaffBills() {
+        return staffBills;
+    }
+
+    public ObservableList<TimeKeeping> getTimeKeepings() {
+        return timeKeepings;
+    }
+
+    public ObservableList<Shift> getShifts() {
+        return shifts;
+    }
+
     public DbAdapterCache(){
+        //region linh
         staffs = FXCollections.observableArrayList();
         settings = new Settings();
         customers = FXCollections.observableArrayList();
+        saleBills = FXCollections.observableArrayList();
+        feedbacks = FXCollections.observableArrayList();
+        //endregion
+        //region lam
+        publishers = FXCollections.observableArrayList();
+        categories = FXCollections.observableArrayList();
+        people = FXCollections.observableArrayList();
+        productInfos = FXCollections.observableArrayList();
+        products = FXCollections.observableArrayList();
+        //endregion
+        //region minh
+        importBills = FXCollections.observableArrayList();
+        otherBills = FXCollections.observableArrayList();
+        staffBills = FXCollections.observableArrayList();
+        timeKeepings = FXCollections.observableArrayList();
+        shifts = FXCollections.observableArrayList();
+        //endregion
     }
 }
