fad6b8a6aeb7a44cde3239a506c102e7bdca289b
Genshin <thanhlam02412002@gmail.com>
2022-06-05 18:14:36 +0700

    update test category

---
+++ src/main/java/com/shopkeeper/lam/database/test2.java
@@ -0,0 +1,58 @@
+package com.shopkeeper.lam.database;
+
+import com.shopkeeper.lam.models.*;
+import com.shopkeeper.mediaone.database.DatabaseAdapter;
+
+import java.time.LocalDate;
+
+public class test2 {
+    public void insertPerson() throws Exception {
+        Person x1=new Person("Lam", LocalDate.of(2002,1,24),"thich choi genshin",1232,JobOfPerson.WRITER);
+        Person x2=new Person("Kamisato Ayaka",LocalDate.of(2000,4,13),"the best",23124,JobOfPerson.ACTOR);
+        Person x3=new Person("Emi Fukada",LocalDate.of(2323,4,2),"dien vien phim gi do",23442,JobOfPerson.ACTOR);
+        Person x4=new Person("DOREMON",LocalDate.of(3424,23,4),"meo may den tu tuong lai",545,JobOfPerson.ACTOR);
+        var adapter = DatabaseAdapter.getDbAdapter();
+        for(var x : adapter.getAllPeople()){
+            System.out.println(x);
+        }
+        adapter.insertPerson(x1);
+        adapter.insertPerson(x2);
+        adapter.insertPerson(x3);
+        adapter.insertPerson(x4);
+        System.out.println("----------<><><><><>----------");
+        for(var x :adapter.getAllPeople()){
+            System.out.println(x);
+        }
+    }
+    public void updatePerson() throws Exception{
+        var adapter = DatabaseAdapter.getDbAdapter();
+        for(var x:adapter.getAllPeople()){
+            System.out.println(x);
+        }
+        var y=adapter.getAllPeople().get(3);
+        y.setName("Kujou Sara");
+        y.setDescription("tuong quan Inazuma");
+
+        adapter.updatePerson(y);
+        for(var x:adapter.getAllPeople() ){
+            System.out.println(x);
+        }
+    }
+    public void deletePeople() throws Exception {
+        var adapter = DatabaseAdapter.getDbAdapter();
+        for(var x:adapter.getAllPeople()){
+            //Nếu như các ô đã triển khai override thuộc tính toString() cho
+            // của ô rồi thì viết thế này
+            System.out.println(x);
+            //Còn nếu không các ô phải in từng thuộc tính 1 ra
+        }
+        var y = adapter.getAllPeople().get(3);
+        adapter.deletePerson(y);
+        for(var x:adapter.getAllPeople()){
+            //Nếu như các ô đã triển khai override thuộc tính toString() cho
+            // của ô rồi thì viết thế này
+            System.out.println(x);
+            //Còn nếu không các ô phải in từng thuộc tính 1 ra
+        }
+    }
+}
--- src/main/java/com/shopkeeper/lam/database/test3.java
+++ src/main/java/com/shopkeeper/lam/database/test3.java
@@ -1,47 +1,50 @@
 package com.shopkeeper.lam.database;
 
-import com.shopkeeper.lam.models.Category;
+import com.shopkeeper.lam.models.*;
 import com.shopkeeper.mediaone.database.DatabaseAdapter;
 
-public class test1 {
-    public void insertCategory() throws Exception {
-        Category honor = new Category("Kinh di","phim rat dang so,khong danh cho tre em duoi 18 tuoi",1244343);
-        Category comedy = new Category("Phim hai","phim rat buon cuoi",213232);
+public class test3 {
+    public void insertPublisher() throws Exception {
+        Publisher p1 = new Publisher("Kim Dong","Viet Nam",3434,"xuat ban Doremon");
+        Publisher p2 = new Publisher("Hoyoverse","Trung Quoc",3343,"genshin,honkai");
+        Publisher p3 = new Publisher("Garena","Trung Quoc",355,"LOL,FIFA 4");
+
         var adapter = DatabaseAdapter.getDbAdapter();
-        for(var x : adapter.getAllCategories()){
+        for(var x : adapter.getAllPublishers()){
             System.out.println(x);
         }
-        adapter.insertCategory(honor);
-        adapter.insertCategory(comedy);
+        adapter.insertPublisher(p1);
+        adapter.insertPublisher(p2);
+        adapter.insertPublisher(p3);
         System.out.println("----------<><><><><>----------");
-        for(var x :adapter.getAllCategories()){
+        for(var x :adapter.getAllPublishers()){
             System.out.println(x);
         }
     }
-    public void updateCategory() throws Exception{
+    public void updatePublisher() throws Exception{
         var adapter = DatabaseAdapter.getDbAdapter();
-        for(var x:adapter.getAllCategories()){
+        for(var x:adapter.getAllPublishers()){
             System.out.println(x);
         }
-        var y=adapter.getAllCategories().get(1);
-        y.setName("anime");
-        y.setDescription("hay nhat the gioi");
-        adapter.updateCategory(y);
-        for(var x:adapter.getAllCategories() ){
+        var y=adapter.getAllPublishers().get(2);
+        y.setName("Bo giao duc");
+        y.setDescription("ko biet noi gi");
+        adapter.updatePublisher(y);
+        for(var x:adapter.getAllPublishers() ){
             System.out.println(x);
         }
     }
     public void deleteCategories() throws Exception {
         var adapter = DatabaseAdapter.getDbAdapter();
-        for(var x:adapter.getAllCategories()){
+        for(var x:adapter.getAllPublishers()){
             //Nếu như các ô đã triển khai override thuộc tính toString() cho
             // của ô rồi thì viết thế này
             System.out.println(x);
             //Còn nếu không các ô phải in từng thuộc tính 1 ra
         }
-        var y = adapter.getAllCategories().get(1);
-        adapter.deleteCategory(y);
-        for(var x:adapter.getAllCategories()){
+        var y = adapter.getAllPublishers().get(2);
+        adapter.deletePublisher(y);
+        for(var x:adapter.getAllPublishers()){
             //Nếu như các ô đã triển khai override thuộc tính toString() cho
             // của ô rồi thì viết thế này
             System.out.println(x);
