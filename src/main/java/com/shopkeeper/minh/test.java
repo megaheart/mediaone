9c9549223146794217ca2ad1369ffaeb5eabf57f
DucMinh <minhmeo7a1@gmail.com>
2022-07-08 11:33:26 +0700

    Seed data shift + attendance + StaffBill

--- src/main/java/com/shopkeeper/minh/test.java
+++ src/main/java/com/shopkeeper/minh/test.java
@@ -19,11 +19,21 @@ import java.util.Random;
 public class test {
     public void insertOtherBills() throws Exception {
         //Tạo 5 đối tượng mẫu với đầy đủ thuộc tính (trừ ID)
-        OtherBill x1 = new OtherBill("Hoa don 1", 1000, LocalDate.of(2022, 6, 2), true, "day la hoa don 1");
-        OtherBill x2 = new OtherBill("Hoa don 2", 2000, LocalDate.of(2022, 6, 3), false, "day la hoa don 2");
-        OtherBill x3 = new OtherBill("Hoa don 3", 3000, LocalDate.of(2022, 6, 4), true, "day la hoa don 3");
-        OtherBill x4 = new OtherBill("Hoa don 4", 4000, LocalDate.of(2022, 6, 5), false, "day la hoa don 4");
-        OtherBill x5 = new OtherBill("Hoa don 5", 5000, LocalDate.of(2022, 6, 6), true, "day la hoa don 5");
+        OtherBill x1 = new OtherBill("Vận chuyển", 1000, LocalDate.of(2022, 6, 2), true, "Không có ghi chú");
+        OtherBill x2 = new OtherBill("Vận chuyển", 2000, LocalDate.of(2022, 6, 3), false, "Không có ghi chú");
+        OtherBill x3 = new OtherBill("Chi phí mặt bằng", 3000, LocalDate.of(2022, 6, 4), true, "Không có ghi chú");
+        OtherBill x4 = new OtherBill("Chi phí mặt bằng", 4000, LocalDate.of(2022, 6, 5), false, "Không có ghi chú");
+        OtherBill x5 = new OtherBill("Chi phí mặt bằng", 5000, LocalDate.of(2022, 6, 6), true, "Không có ghi chú");
+        OtherBill x6 = new OtherBill("Vận chuyển", 1000, LocalDate.of(2022, 6, 2), true, "Không có ghi chú");
+        OtherBill x7 = new OtherBill("Vận chuyển", 2000, LocalDate.of(2022, 6, 3), false, "Không có ghi chú");
+        OtherBill x8 = new OtherBill("Chi phí mặt bằng", 3000, LocalDate.of(2022, 6, 4), true, "Không có ghi chú");
+        OtherBill x9 = new OtherBill("Chi phí mặt bằng", 4000, LocalDate.of(2022, 6, 5), false, "Không có ghi chú");
+        OtherBill x10 = new OtherBill("Chi phí mặt bằng", 5000, LocalDate.of(2022, 6, 6), true, "Không có ghi chú");
+        OtherBill x11 = new OtherBill("Vận chuyển", 1000, LocalDate.of(2022, 6, 2), true, "Không có ghi chú");
+        OtherBill x12 = new OtherBill("Vận chuyển", 2000, LocalDate.of(2022, 6, 3), false, "Không có ghi chú");
+        OtherBill x13 = new OtherBill("Vận chuyển", 3000, LocalDate.of(2022, 6, 4), true, "Không có ghi chú");
+        OtherBill x14 = new OtherBill("Vận chuyển", 4000, LocalDate.of(2022, 6, 5), false, "Không có ghi chú");
+        OtherBill x15 = new OtherBill("Chi phí mặt bằng", 5000, LocalDate.of(2022, 6, 6), true, "Không có ghi chú");
 
         var adapter = DatabaseAdapter.getDbAdapter();
         for (var x : adapter.getAllOtherBills()) {
@@ -38,57 +48,7 @@ public class test {
         adapter.insertOtherBill(x4);
         adapter.insertOtherBill(x5);
 
-        System.out.println("----------<><><><><>----------");
-        for (var x : adapter.getAllOtherBills()) {
-            //Nếu như các ô đã triển khai override thuộc tính toString() cho
-            // của ô rồi thì viết thế này
-            System.out.println(x);
-            //Còn nếu không các ô phải in từng thuộc tính 1 ra
-        }
-    }
 
-    public void updateOtherBills() throws Exception {
-        var adapter = DatabaseAdapter.getDbAdapter();
-        for (var x : adapter.getAllOtherBills()) {
-            //Nếu như các ô đã triển khai override thuộc tính toString() cho
-            // của ô rồi thì viết thế này
-            System.out.println(x);
-            //Còn nếu không các ô phải in từng thuộc tính 1 ra
-        }
-
-        var x4 = adapter.getAllOtherBills().get(3);
-        //Update toàn bộ các thuộc tính trừ ID
-        x4.setName("XXXXXXXXXX");
-        x4.setIsEffected(true);
-        x4.setPrice(4500);
-        x4.setNote("Updated bill");
-        x4.setTime(LocalDate.of(2022, 6, 14));
-        adapter.updateOtherBill(x4);
-
-        for (var x : adapter.getAllOtherBills()) {
-            //Nếu như các ô đã triển khai override thuộc tính toString() cho
-            // của ô rồi thì viết thế này
-            System.out.println(x);
-            //Còn nếu không các ô phải in từng thuộc tính 1 ra
-        }
-    }
-
-    public void deleteOtherBills() throws Exception {
-        var adapter = DatabaseAdapter.getDbAdapter();
-        for (var x : adapter.getAllOtherBills()) {
-            //Nếu như các ô đã triển khai override thuộc tính toString() cho
-            // của ô rồi thì viết thế này
-            System.out.println(x);
-            //Còn nếu không các ô phải in từng thuộc tính 1 ra
-        }
-        var x3 = adapter.getAllOtherBills().get(2);
-        adapter.deleteOtherBill(x3);
-        for (var x : adapter.getAllOtherBills()) {
-            //Nếu như các ô đã triển khai override thuộc tính toString() cho
-            // của ô rồi thì viết thế này
-            System.out.println(x);
-            //Còn nếu không các ô phải in từng thuộc tính 1 ra
-        }
     }
 
     public void insertImportBills() throws Exception {
