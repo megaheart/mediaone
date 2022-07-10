d2114b3bd0aac1149e0fcec8c22247f3412ca533
megaheart <21360138+megaheart@users.noreply.github.com>
2022-07-10 12:48:53 +0700

    little update  :)

--- src/main/java/com/shopkeeper/linh/seeders/DataSeeder.java
+++ src/main/java/com/shopkeeper/linh/seeders/DataSeeder.java
@@ -2,6 +2,7 @@ package com.shopkeeper.linh.seeders;
 
 import com.shopkeeper.lam.database.*;
 import com.shopkeeper.linh.models.Feedback;
+import com.shopkeeper.mediaone.database.DatabaseAdapter;
 import com.shopkeeper.minh.models.OtherBill;
 
 public class DataSeeder {
@@ -31,5 +32,27 @@ public class DataSeeder {
         new testProduct().insert();
         //load 4
         FeedbackSeeder.SeedData();
+
+        var adapter = DatabaseAdapter.getDbAdapter();
+        for(var importBill:adapter.getAllImportBills()){
+            importBill.setPrice(0);
+        }
+        for(var saleBill:adapter.getAllSaleBills()){
+            saleBill.setPrice(0);
+        }
+        for(var product:adapter.getAllProducts()){
+            if(product.getImportBill() != null){
+                product.getImportBill().setPrice(product.getImportBill().getPrice() + product.getImportCost());
+            }
+            if(product.getSaleBill() != null){
+                product.getSaleBill().setPrice(product.getSaleBill().getPrice() + product.getSaleValue());
+            }
+        }
+        for(var importBill:adapter.getAllImportBills()){
+            adapter.updateImportBill(importBill);
+        }
+        for(var saleBill:adapter.getAllSaleBills()){
+            adapter.updateSaleBill(saleBill);
+        }
     }
 }
