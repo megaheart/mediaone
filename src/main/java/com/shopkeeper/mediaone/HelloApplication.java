7547f84f30be8ba8d142c2c1e6f1f9bd375f071d
DucMinh <minhmeo7a1@gmail.com>
2022-07-21 14:01:59 +0700

    Test

--- src/main/java/com/shopkeeper/mediaone/HelloApplication.java
+++ src/main/java/com/shopkeeper/mediaone/HelloApplication.java
@@ -31,6 +31,20 @@ public class HelloApplication extends Application {
     }
 
     public static void main(String[] args) throws Exception {
+        var adapter = DatabaseAdapter.getDbAdapter();
+        ObservableList<Staff> staffs = adapter.getAllStaffs();
+        StaffBill staffBill = null;
+        Random random = new Random(0);
+        double workHours;
+        LocalDate from;
+        for (Staff staff: staffs){
+            int extra = random.nextInt(10);
+            from = StaffManager.getManager().getFrom(staff);
+            workHours = StaffManager.getManager().getWorkHours(staff);
+            staffBill = new StaffBill("Lương tháng 4 cho " + staff.getName(), 20 * workHours, LocalDate.of(2022, 4, 30), true, "Không có ghi chú",
+                    from, staff, 20, workHours);
+            adapter.insertStaffBill(staffBill);
+        }
         launch();
     }
 }
