51f14f88d8f85029f9e18f6c5a96c34733449449
Genshin <thanhlam02412002@gmail.com>
2022-07-22 16:38:38 +0700

    something

--- src/main/java/com/shopkeeper/lam/windowfactories/fileFxml/WindowManagerController.java
+++ src/main/java/com/shopkeeper/lam/windowfactories/fileFxml/WindowManagerController.java
@@ -55,7 +55,29 @@ public class WindowManagerController  {
     }
     public void setDataSeed(){
         try {
-            DataSeeder.SeedData();
+            Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+            alert.setTitle("Cảnh báo");
+            alert.setHeaderText("Thao tác này chỉ dùng để thử nghiệm sản phẩm.");
+            alert.setContentText("Nếu tiếp tục thì dữ liệu hiện có của bạn sẽ bị ảnh hưởng.\nBạn có chắc chắn muốn tiếp tục không?");
+            ButtonType buttonTypeYes = new ButtonType("Có");
+            ButtonType buttonTypeNo = new ButtonType("Không");
+            alert.getButtonTypes().setAll(buttonTypeYes, buttonTypeNo);
+            Optional<ButtonType> result = alert.showAndWait();
+            if (result.get() == buttonTypeYes){
+                DataSeeder.SeedData();
+                alert.close();
+                Alert alert1 = new Alert(Alert.AlertType.INFORMATION);
+                alert1.setTitle("Thông báo");
+                alert1.setHeaderText("Thao tác thành công");
+                alert1.setContentText("Dữ liệu đã được thêm vào.");
+                alert1.showAndWait();
+            }
+            else {
+                alert.close();
+            }
+
+
+
         }
         catch (Exception exception){
             //exception.printStackTrace();
